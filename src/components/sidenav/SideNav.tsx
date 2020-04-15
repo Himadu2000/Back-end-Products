6e25526c910bc2842364972b79790a272b450780
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-04-16 05:26:33 +0700

    Update

---
+++ src/components/sidenav/SideNav.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { MDBBtn, MDBIcon, MDBContainer, MDBCol, MDBRow } from "mdbreact";
+import "./SideNav.css";
+
+const SideNav = () => {
+  const [sideNavOpen, setSideNavOpen] = useState(false);
+  const [sideNavOpenIcon, setSideNavOpenIcon] = useState("angle-double-right");
+  const [openCloseStyle, setOpenCloseStyle] = useState("close");
+  function sideNavOpenEvent() {
+    if (sideNavOpen) {
+      setOpenCloseStyle("close");
+      setSideNavOpenIcon("angle-double-left");
+      setSideNavOpen(false);
+    } else {
+      setOpenCloseStyle("open");
+      setSideNavOpenIcon("angle-double-right");
+      setSideNavOpen(true);
+    }
+  }
+  return (
+    <>
+      <div className="sidenav">
+        <a href="">
+          <MDBIcon icon="calculator" size="2x" />
+        </a>
+        <a href="">
+          <MDBIcon icon="calculator" size="2x" />
+        </a>
+        <a href="">
+          <MDBIcon icon="swatchbook" size="2x" />
+        </a>
+        <div className="bottom">
+          <a href="">
+            <MDBIcon icon="calculator" size="2x" />
+          </a>
+          <a href="">
+            <MDBIcon icon="calculator" size="2x" />
+          </a>
+          <a href="">
+            <MDBIcon icon="swatchbook" size="2x" />
+          </a>
+        </div>
+      </div>
+      {/*
+      <div className="secondsidenav">
+        <div className={openCloseStyle}>
+          <MDBContainer>
+            <MDBBtn className="Button" color="dark">
+              <MDBRow>
+                <MDBCol sm="2">
+                  <MDBIcon icon={sideNavOpenIcon} />
+                </MDBCol>
+                <MDBCol sm="8">This is a cat</MDBCol>
+              </MDBRow>
+            </MDBBtn>
+            <MDBBtn className="Button" color="dark">
+              <MDBRow>
+                <MDBCol sm="2">
+                  <MDBIcon icon={sideNavOpenIcon} />
+                </MDBCol>
+                <MDBCol sm="8">This is a cat</MDBCol>
+              </MDBRow>
+            </MDBBtn>
+
+            <MDBBtn onClick={sideNavOpenEvent} className="Button" color="dark">
+              <MDBRow>
+                <MDBCol sm="2">
+                  <MDBIcon icon={sideNavOpenIcon} />
+                </MDBCol>
+                <MDBCol sm="8">Close</MDBCol>
+              </MDBRow>
+            </MDBBtn>
+          </MDBContainer>
+        </div>
+</div> */}
+    </>
+  );
+};
+
+export default SideNav;
